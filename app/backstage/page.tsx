42c7093ece45a5d9291d340d683852fb7298b68b
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-19 23:09:40 +0100

    Update page.tsx

--- app/backstage/page.tsx
+++ app/backstage/page.tsx
@@ -1,1 +1,17 @@
-
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "40px",
+        textAlign: "center",
+      }}
+    >
+      <p>
+        Our backstage area will open soon.
+      </p>
+    </div>
+  );
+}

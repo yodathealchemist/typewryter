ceccf8ced6ed09cf76c9ac4191715837c980e2be
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-19 19:47:00 +0100

    Create page.tsx

---
+++ app/imprint/page.tsx
@@ -0,0 +1,9 @@
+export default function Imprint() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Imprint</h1>
+      <p>Here is the imprint for TypeWryter.</p>
+      {/* Add your imprint details here */}
+    </div>
+  );
+}

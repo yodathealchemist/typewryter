e86718f032998ccd836ea4756c07bec96fad9fa7
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-19 19:46:22 +0100

    Create page.tsx

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+export default function PrivacyPolicy() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Privacy Policy</h1>
+      <p>Here is the privacy policy for TypeWryter.</p>
+      {/* Add your privacy policy details here */}
+    </div>
+  );
+}

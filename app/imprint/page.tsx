4e6a80da743db531152735b344d2c91aea8b3627
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-22 13:09:46 +0100

    Update page.tsx

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -1,11 +1,13 @@
 export default function Imprint() {
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Imprint/Impressum</h1>
+      <h1>Imprint (Impressum)</h1>
+      <p></p>
       <p>Dr. Yuno Rolfes</p>
       <p>Albert-Schweitzer-Str. 22</p>
       <p>32602 Vlotho</p>
       <p>Germany</p>
+      <p></p>
       <p>Email: y_1@riseup.net</p>
       <p>Phone: +49(0) 176 31 26 10 77</p>
     </div>

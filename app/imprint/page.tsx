465b2786eee25646d42f06e84e349fc9baa4437a
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-22 14:39:26 +0100

    Update page.tsx

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -2,10 +2,10 @@ export default function Imprint() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Imprint (Impressum)</h1>
-      <p><br>Dr. Yuno Rolfes<br>
-      Albert-Schweitzer-Str. 22<br>
-      32602 Vlotho<br>
-      Germany<br><br>
+      <p><br />Dr. Yuno Rolfes<br />
+      Albert-Schweitzer-Str. 22<br />
+      32602 Vlotho<br />
+      Germany<br /><br />
       Email: typewryter@gmx.net</p>
     </div>
   );

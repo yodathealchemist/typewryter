6a284e61cf54cf6f3dfd97e3ef91a8e3e72bf45e
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-24 23:35:39 +0100

    Update page.tsx

--- app/stories/clockmakerscurse/chapter3/page.tsx
+++ app/stories/clockmakerscurse/chapter3/page.tsx
@@ -41,7 +41,7 @@ export default function Story() {
           textAlign: "left",
         }}
       >
-<h2 style="text-align: center;">Chapter 3: Shadows</h2>
+<h2 style={{ textAlign: "center" }}>Chapter 3: Shadows</h2>
 
         
 

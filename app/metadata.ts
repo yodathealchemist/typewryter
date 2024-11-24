487c3f857f20e3478ceb545c0b2c4c4a7ea53347
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-24 20:45:25 +0100

    Create metadata.ts

---
+++ app/metadata.ts
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "type_wryter | Interactive Storytelling Platform",
+  description: "Engage with immersive, interactive stories across various genres.",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  themeColor: "#ffffff",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+};

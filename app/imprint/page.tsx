5f30305b1548751bfdaef18f7aed835d0632006d
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-25 12:42:05 +0100

    Update page.tsx

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -11,6 +11,24 @@ export default function Imprint() {
       Email: typewryter@gmx.net<br />
       Phone: +49(0) 5733 / 95371</p>
 
+<p>The laws of the Federal Republic of Germany apply to any legal disputes arising from the use of this website.</p>
+
+<h2> style={{ fontSize: "1.5rem" }}>Exclusion of Liability – Disclaimer</h2>
+<h3 style={{ fontSize: "1rem" }}>Liability for Content:</h3>
+
+<p>All content on our website has been created with the utmost care and to the best of our knowledge. However, we cannot accept any liability for the accuracy, completeness and topicality of the content. As a service provider, we are responsible for our own content on these pages. We are not obliged, however, to monitor transmitted or stored third-party information or to actively investigate circumstances that indicate illegal activity. Obligations to remove or block the use of information in accordance with general legislation remain unaffected by this.</p>
+<p>However, liability in this respect is only possible from the time we become aware of a specific infringement. If we become aware of the above-mentioned infringements, we will remove this content immediately.</p>
+
+<h3 style={{ fontSize: "1rem" }}>Limitation of Liability for External Links:</h3>
+
+<p>Our website contains links to external third-party websites. We have no influence on the content of these directly or indirectly linked websites. Therefore, we cannot accept any liability for the accuracy of the content of the ‘external links’. The respective providers or operators (authors) of the pages are responsible for the content of the external links.</p>
+<p>The external links were checked for possible legal violations at the time the links were created and were free of illegal content at the time the links were created. It is not possible to constantly check the content of external links without concrete evidence of an infringement. In the case of direct or indirect links to third-party websites that lie outside our area of responsibility, a liability obligation would only exist if we were aware of the content and it would be technically possible and reasonable for us to prevent use in the event of illegal content.</p>
+<p></p>This disclaimer also applies to links and references set by commentators within our own website. For illegal, incorrect or incomplete contents and in particular for damages resulting from the use or non-use of such information, only the service providers of the pages to which reference is made are liable, not those who merely refer to the respective publications via links.</p>
+<p>If we become aware of any legal infringements, we will remove the external links immediately.</p>
+
+<h2> style={{ fontSize: "1.5rem" }}>Copyright</h2>
+<p>The content and works published on our website are subject to German copyright law. The reproduction, editing, distribution and any kind of utilisation of the intellectual property in an ideal or material sense of the copyright holder outside the limits of copyright law require the prior written consent of the respective copyright holder within the meaning of copyright law. Downloads and copies of this site are only permitted for private and non-commercial use. If the content on our website was not created by us, the copyrights of third parties must be observed. Third-party content is labelled as such. Should you nevertheless become aware of an infringement of copyright, please inform us accordingly. As soon as we become aware of any legal infringements, we will remove such content immediately.</p>
+
 <h1><br /><br />Privacy Policy</h1>
 
 <h2 style={{ fontSize: "1.5rem" }}>1. Introduction</h2>

958f1d2705e6bc05efb33b140b6a149218d7d536
yodathealchemist <83399859+yodathealchemist@users.noreply.github.com>
2024-11-25 18:56:10 +0100

    Update page.tsx

--- app/stories/clockmakerscurse/page.tsx
+++ app/stories/clockmakerscurse/page.tsx
@@ -29,7 +29,7 @@ export default function Story() {
         }}
         className="top-ad"
       >
-        <p style={{ color: "#999" }}>728x90 Ad Placeholder</p>
+        <p style={{ color: "#999" }}>Ad Placeholder</p>
       </div>
       
       <h1>The Clockmaker&apos;s Curse</h1>
@@ -79,7 +79,7 @@ export default function Story() {
         }}
         className="top-ad"
       >
-        <p style={{ color: "#999" }}>728x90 Ad Placeholder</p>
+        <p style={{ color: "#999" }}>Ad Placeholder</p>
       </div>
 
       <div style={{ width: "300px" }}>
@@ -124,7 +124,7 @@ export default function Story() {
         }}
         className="bottom-ad"
       >
-        <p style={{ color: "#999" }}>970x250 Ad Placeholder</p>
+        <p style={{ color: "#999" }}>Ad Placeholder</p>
       </div>
     </div>
   );
